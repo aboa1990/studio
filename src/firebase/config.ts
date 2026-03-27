e90034042a3656f594eb5626f3c182b66bc5e261
abdulla Ali <abo.roseware@gmail.com>
2026-03-27 08:03:53 +0000

    ySet up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1067073093-b642b",
+  "appId": "1:314252403906:web:5d3a4f803657361b36c75b",
+  "apiKey": "AIzaSyDK8ALHRfgauK4fXDwa8RMeJutJ1PeLsyc",
+  "authDomain": "studio-1067073093-b642b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "314252403906"
+};
